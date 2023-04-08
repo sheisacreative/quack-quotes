a9469934c15c277d76e33aeee99f373c0f6dcb4b
Maiane Gabriele <maianedearaujo@gmail.com>
2023-04-08 13:43:59 -0300

    Tags container position and overflow solved
    `

--- components/tag/TagContainer.js
+++ components/tag/TagContainer.js
@@ -4,24 +4,34 @@ import styles from "./TagContainer.module.css";
 
 const TagContainer = () => {
   return (
-    <ul className={`container ${styles.tagContainer}`}>
-      <SingleTag />
-      <SingleTag />
-      <SingleTag />
-      <SingleTag />
-      <SingleTag />
-      <SingleTag />
-      <SingleTag />
-      <SingleTag />
-      <SingleTag />
-      <SingleTag />
-      <SingleTag />
-      <SingleTag />
-      <SingleTag />
-      <SingleTag />
-      <SingleTag />
-      <SingleTag />
-    </ul>
+    <div className={styles.relativePosition}>
+      <ul className={`${styles.tagContainer}`}>
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+        <SingleTag />
+      </ul>
+    </div>
   );
 };
 
