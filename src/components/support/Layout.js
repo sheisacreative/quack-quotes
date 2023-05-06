1f9a22718df05e3b29a8e8b1a1c8531ddbe2cc34
Maiane Gabriele <maianedearaujo@gmail.com>
2023-05-06 17:03:14 -0300

    Removed header and footer from single quote page

--- src/components/support/Layout.js
+++ src/components/support/Layout.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Navbar from "../sections/Navbar";
 import Footer from "../sections/Footer";
+import { useRouter } from "next/router";
 
 const Layout = ({ children }) => {
+  const router = useRouter();
+
+  if (router.pathname.startsWith(`/quote`)) {
+    return <>{children}</>;
+  }
+
   return (
     <>
       <Navbar />
