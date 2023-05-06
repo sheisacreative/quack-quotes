435a06fb3097e85ce9b7dadc4a521b47fe773981
Maiane Gabriele <maianedearaujo@gmail.com>
2023-05-06 15:09:15 -0300

    Tag scroll and show tags working mobile and desktop. Fixes #53

--- src/components/tag/TagContainer.js
+++ src/components/tag/TagContainer.js
@@ -1,20 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import SingleTag from "./SingleTag";
 import styles from "./TagContainer.module.css";
 import { tags } from "@/src/data/data";
 
 const TagContainer = () => {
   const [showTags, setShowTags] = useState(false);
-  const [width, setWidth] = useState(1000);
+  const [width, setWidth] = useState(0);
   const [isMobile, setIsMobile] = useState(false);
+  const [containerHeight, setContainerHeight] = useState(0);
+  const [containerWidth, setContainerWidth] = useState(0);
+  const tagsListContainer = useRef();
+  console.log(width);
+  console.log(isMobile);
+
+  useEffect(() => {
+    const height = tagsListContainer.current.offsetHeight;
+    const width = tagsListContainer.current.offsetWidth;
+    setContainerHeight(height);
+    setContainerWidth(width);
+  }, [tagsListContainer]);
 
   function handleWindowSizeChange() {
     setWidth(window.innerWidth);
-    console.log(window.innerWidth);
   }
   useEffect(() => {
-    window.addEventListener("resize", handleWindowSizeChange);
+    setWidth(window.innerWidth);
 
+    window.addEventListener("resize", handleWindowSizeChange);
     return () => {
       window.removeEventListener("resize", handleWindowSizeChange);
     };
@@ -22,20 +34,27 @@ const TagContainer = () => {
 
   useEffect(() => {
     if (width <= 768) {
-      console.log("is mobile");
       setIsMobile(true);
     } else {
-      console.log("not mobile");
       setIsMobile(false);
     }
   }, [width]);
 
   if (tags) {
     return (
-      <ul
-        className={`${styles.tagContainer} ${
-          !isMobile && `container ${styles.containerDesktop}`
-        } ${showTags && styles.showTags} `}
+      <div
+        className={`container ${isMobile && styles.isMobile} ${
+          styles.tagsContainer
+        }`}
+        style={{
+          height: showTags && `${containerHeight}px`,
+          marginLeft:
+            isMobile &&
+            `calc(((100vw - (100vw - 100%)) - ${containerWidth}px) / 2)`,
+          width:
+            isMobile &&
+            `calc(100vw - (((100vw - (100vw - 100%)) - ${containerWidth}px) / 2))`,
+        }}
       >
         {!isMobile && (
           <button
@@ -45,11 +64,12 @@ const TagContainer = () => {
             {showTags ? `Colapsar tags` : `Todas as tags`}
           </button>
         )}
-
-        {tags.map((tag, index) => {
-          return <SingleTag key={index} {...tag} />;
-        })}
-      </ul>
+        <ul className={`${styles.tagsList}`} ref={tagsListContainer}>
+          {tags.map((tag, index) => {
+            return <SingleTag key={index} {...tag} />;
+          })}
+        </ul>
+      </div>
     );
   }
 
