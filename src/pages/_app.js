f7abc2e742f4e3b227f928b19418fbc81c444ed1
Maiane Gabriele <maianedearaujo@gmail.com>
2023-05-20 16:01:36 -0300

    Analytics added to app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,8 @@ import styles from "./_app.module.css";
 import { favicon } from "../../public/favicon.ico";
 import Layout from "../components/support/Layout";
 import NavigationProvider from "../contexts/navigationContext";
+import Script from "next/script";
+import * as gtag from "../utils/gtag";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -18,6 +20,27 @@ export default function App({ Component, pageProps }) {
         />
         <link rel="icon" type="image/x-icon" href={favicon} />
       </Head>
+      {/* Google tag (gtag.js) */}
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
       <NavigationProvider>
         <Layout>
           <div className={styles.pageContainer}>
